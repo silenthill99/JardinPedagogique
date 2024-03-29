37c4677fc5727f6370590806c24c928ea21af5ed
silenthill99 <florian.graziani@sfr.fr>
2024-03-29 23:24:35 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './css/App.css'
+import Home from "./pages/Home.jsx";
+
+function App() {
+    return (
+        <>
+            <Home/>
+        </>
+    )
+}
+
+export default App
---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import "../css/Home.css"
+
+const Home = () => {
+    return (
+        <div>
+            <h1>Page d'accueil</h1>
+            <section className="grid">
+                <div className="test">
+                    Essai
+                </div>
+                <div className="test"></div>
+                <div className="test"></div>
+                <div className="test"></div>
+                <div className="test"></div>
+                <div className="test"></div>
+                <div className="test"></div>
+                <div className="test"></div>
+                <div className="test"></div>
+            </section>
+        </div>
+    );
+};
+
+export default Home;
