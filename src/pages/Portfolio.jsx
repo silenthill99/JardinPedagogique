8beb64296c52b93ee6fdc5c537c5ba44ace8aadc
silenthill99 <florian.graziani@sfr.fr>
2024-04-07 22:27:24 +0200

    Rajout de photos + changement de certains noms

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -10,7 +10,7 @@ const Portfolio = () => {
             }}>Portfolio</h1>
             <div className="portfolio">
                 {imageLink.map((image) => (
-                    <a key={image.alt} href={image.link} target={"_blank"}>
+                    <a key={image.id} href={image.link} target={"_blank"}>
                         <img src={image.link} alt={image.alt}/>
                         <div>
                             Voir la photo
@@ -24,16 +24,39 @@ const Portfolio = () => {
 
 const imageLink = [
     {
-        link: "23 mars  - 3e Forum Citoyen Responsabilité écologique.png",
-        alt: "Affiche"
+        link: "1 - 23 mars  - 3e Forum Citoyen Responsabilité écologique.png",
+        alt: "Affiche",
+        id: 1
     },
     {
-        link: "418171939_1376080656561973_1780255866686462083_n.jpg",
-        alt: "Tableau"
+        link: "2 - 418171939_1376080656561973_1780255866686462083_n.jpg",
+        alt: "Tableau",
+        id: 2
     },
     {
-        link: "419044626_1376080719895300_3984056632727648427_n.jpg",
-        alt: "Présentation"
+        link: "3 - 419044626_1376080719895300_3984056632727648427_n.jpg",
+        alt: "Présentation",
+        id: 3
+    },
+    {
+        link: "4 - 419324800_1376080636561975_3928395571157246398_n.jpg",
+        alt: "Atelier",
+        id: 4
+    },
+    {
+        link: "5 - 420203580_1376080626561976_4037523779686633588_n.jpg",
+        alt: "Atelier 2",
+        id: 5
+    },
+    {
+        link: "6 - 420478381_1376080606561978_873995250332830573_n.jpg",
+        alt: "Atelier 3",
+        id: 6
+    },
+    {
+        link: "7 - IMG_20240328_155410.jpg",
+        alt: "Cabanon",
+        id: 7
     }
 ]
 
