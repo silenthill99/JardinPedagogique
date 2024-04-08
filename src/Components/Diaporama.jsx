3ca1c25b3bf0006e8e0c7f50ceba8afb71532b44
BuildTools <unconfigured@null.spigotmc.org>
2024-04-08 15:06:22 +0200

    Création d'un diaporama

--- src/Components/Diaporama.jsx
+++ src/Components/Diaporama.jsx
@@ -3,9 +3,39 @@ import "../css/Diaporama.css"
 import { FaArrowAltCircleLeft } from "react-icons/fa";
 import { FaArrowAltCircleRight } from "react-icons/fa";
 
+
+let compteur = 0;
+let timer, elements, slides, slideWidth;
+window.onload = () => {
+    const diapo = document.querySelector(".diapo")
+    elements = document.querySelector(".elements")
+    slides = Array.from(elements.children)
+
+    // Calcul de la largeur du diapo
+    slideWidth = diapo.getBoundingClientRect().width
+}
+
+function slideNext() {
+    compteur++
+    if (compteur == slides.length) {
+        compteur = 0
+    }
+    let decal = -slideWidth * compteur
+    elements.style.transform = `translateX(${decal}px)`
+}
+
+function slidePrev() {
+    compteur--;
+    if (compteur < 0) {
+        compteur = slides.length-1
+    }
+    let decal = -slideWidth * compteur
+    elements.style.transform = `translateX(${decal}px)`
+}
+
 const Diaporama = () => {
     return (
-        <div className={"diapo"}>
+        <div className="diapo">
             <div className="elements">
                 <div className="element active">
                     <img src={"3 - 419044626_1376080719895300_3984056632727648427_n.jpg"} alt="Image 1"/>
@@ -22,8 +52,8 @@ const Diaporama = () => {
                     </div>
                 </div>
             </div>
-            <FaArrowAltCircleLeft id="nav-gauche" className="las la-chevron-left"/>
-            <FaArrowAltCircleRight id="nav-droite" className="las la-chevron-right"/>
+            <FaArrowAltCircleLeft id="nav-gauche" onClick={slidePrev} className="las la-chevron-left"/>
+            <FaArrowAltCircleRight id="nav-droite" onClick={slideNext} className="las la-chevron-right"/>
         </div>
     );
 };
