982f809f85e0e94941efa18d04e2c2f3bca573eb
BuildTools <unconfigured@null.spigotmc.org>
2024-04-08 15:14:36 +0200

    Rajout d'une div + les définitions

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,26 +5,34 @@ import Diaporama from "../Components/Diaporama.jsx";
 const Home = () => {
     document.title = "Accueil | Jardin pédagogique"
     return (
-        <div>
+        <>
             <h1>Page d'accueil</h1>
             <h2 style={{
                 textDecoration:"underline"
             }}>Contexte</h2>
             <div className="contexte">
-                <p style={{
-                    textAlign:"justify"
-                }}>Le samedi 23 Mars 2024, les stagiaires de l'école de la 2ème chance de Cosne-Cours sur loire ont été
-                    invités pour participer à un projet communal lié à l'environment. Parmi les différents projets qui
-                    ont été proposés, celui de faire un jardin partagé a été retenu.
-                <ul>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                </ul>
-                </p>
+                <div>
+                    <p style={{
+                        textAlign: "justify"
+                    }}>Le samedi 23 Mars 2024, les stagiaires de l'école de la 2ème chance de Cosne-Cours sur loire ont
+                        été
+                        invités pour participer à un projet communal lié à l'environment. Parmi les différents projets
+                        qui
+                        ont été proposés, celui de faire un jardin partagé a été retenu.
+
+                    </p>
+                    <h2>Quelques définitions d'un jardin partagé</h2>
+                    <ul>
+                        <li>Lieu de vie ouvert sur le quartier, convivial, qui favorise les rencontres entre
+                            générations et entre cultures.</li>
+                        <li>Terrain cultivable ou en friche mis à la disposition d’habitants d’une ville qui ne
+                            bénéficient pas d’espace de verdure.</li>
+                        <li>Jardin qui se conçoit, se construit et se cultive collectivement.</li>
+                    </ul>
+                </div>
                 <Diaporama/>
             </div>
-        </div>
+        </>
     );
 };
 
