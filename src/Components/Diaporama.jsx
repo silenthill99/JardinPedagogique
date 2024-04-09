0c91ee4daa1e69aac5ae5dba319517b8940ede96
BuildTools <unconfigured@null.spigotmc.org>
2024-04-09 16:27:47 +0200

    Mise à jour du diaporama de la page d'accueil

--- src/Components/Diaporama.jsx
+++ src/Components/Diaporama.jsx
@@ -1,10 +1,35 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css"
+import {Carousel} from "react-responsive-carousel";
+import "../css/Diaporama.css"
 
 const Diaporama = () => {
+    const datas = [
+        {
+            id: 1,
+            image: "3 - 419044626_1376080719895300_3984056632727648427_n.jpg",
+            titre: "Image du slider 1",
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, necessitatibus!"
+        },
+        {
+            id: 2,
+            image: "4 - 419324800_1376080636561975_3928395571157246398_n.jpg",
+            titre: "Image du slider 2",
+            text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, necessitatibus!"
+        }
+    ]
     return (
-        <div>
-            slider
-        </div>
+        <Carousel autoPlay infiniteLoop interval={6000} thumbWidth={120} showIndicators={false}>
+            {datas.map((slide) => (
+                <div key={slide.id}>
+                    <img src={slide.image} alt=""/>
+                    <div className="overlay">
+                        <h2 className="overlay_title">{slide.titre}</h2>
+                        <p className="overlay_text">{slide.text}</p>
+                    </div>
+                </div>
+            ))}
+        </Carousel>
     );
 };
 
